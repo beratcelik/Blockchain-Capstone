7804522f65277bb18b042c1744bff6af95bd49c4
Berat Çelik <beratcelik@gmail.com>
2021-06-15 19:42:26 +0300

    Editted CustomERC721Token with the comments of Lorenzo

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -544,18 +544,19 @@ contract ERC721Metadata is ERC721Enumerable, usingOraclize {
 //      -calls the superclass mint and setTokenURI functions
 
 contract CustomERC721Token is ERC721Metadata {
-    string _baseTokenURI = "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/";
+   // string baseTokenURI = "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/";
    // string name = "CustomToken";
    // string symbol = "cToken";
-    constructor(string memory _name, string memory _symbol)
-    ERC721Metadata(_name, _symbol, _baseTokenURI)
+    constructor(string memory name, string memory symbol)
+    ERC721Metadata(name, symbol, "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/")
     public
     {
     }
 
   function mint(address to, uint256 tokenId) public onlyOwner returns(bool){
         super._mint(to, tokenId);
-        super.setTokenURI(tokenId, _baseTokenURI);
+        setTokenURI(tokenId);
+        //super.setTokenURI(tokenId, _baseTokenURI);
         return true;
     }
 
